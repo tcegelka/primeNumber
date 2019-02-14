61a22b7bb86b71402ce89d6e2af79e7814f4fa96
tcegelka <ted.cegelka@gmail.com>
2019-02-13 22:10:54 -0500

    Initial commit

---
+++ src/Prime.java
@@ -0,0 +1,13 @@
+import java.util.List;
+
+public class Prime implements PrimeNumberGenerator {
+    @Override
+    public List<Integer> generate(int startingValue, int endingValue) {
+        return null;
+    }
+
+    @Override
+    public boolean isPrime(int value) {
+        return false;
+    }
+}
---
+++ test/PrimeTest.java
@@ -0,0 +1,38 @@
+import static org.junit.jupiter.api.Assertions.assertFalse;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+class PrimeTest {
+    private Prime tester = new Prime();
+
+    @Test
+    void zeroIsNotPrime() {
+        assertFalse(tester.isPrime(0));
+    }
+
+    @Test
+    void oneIsNotPrime() {
+        assertFalse(tester.isPrime(1));
+    }
+
+    @Test
+    void twoIsPrime() {
+        assertTrue(tester.isPrime(2));
+    }
+
+    @Test
+    void threeIsPrime() {
+        assertTrue(tester.isPrime(3));
+    }
+
+    @Test
+    void negativeIsNotPrime() {
+        assertFalse(tester.isPrime(-2));
+    }
+
+    @Test
+    void largerNumberIsPrime() {
+        assertTrue(tester.isPrime(7901));
+    }
+}
